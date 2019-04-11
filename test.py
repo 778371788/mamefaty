089deb67d27c7ea590abf4fc8712246076cb6efb
mfandao <49404111+778371788@users.noreply.github.com>
2019-04-11 13:47:51 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from random import*
 n= randint(0,9)
 b= input("donner un nombre comprise entre 0 et 9")
-if a==b:
+if n==b:
 	print("félicitation")
 else:
 	print("veillez saisir un autre nonbre svp")
